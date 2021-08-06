c418b54167ed994e2801ae90a7355f0e9e13fd9e
SovaV <sova.volodymyr95@gmail.com>
2021-08-06 11:03:33 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,47 +1,62 @@
-const refs = {
-  startBtn: document.querySelector("#start"),
-  stopBtn: document.querySelector("#stop"),
-  days: document.querySelector('.value[data-value="days"]'),
-  hours: document.querySelector('.value[data-value="hours"]'),
-  mins: document.querySelector('.value[data-value="mins"]'),
-  secs: document.querySelector('.value[data-value="secs"]'),
-  timers: document.getElementById("timer-1"),
-};
-
 class CountdownTimer {
-  constructor(date) {
+  constructor({ date }) {
+    this.timerId = null;
     this.date = date;
+    this.startBtn = document.querySelector("#start");
+    this.stopBtn = document.querySelector("#stop");
+    this.sec = document.querySelector('.value[data-value="secs"]');
+    this.min = document.querySelector('.value[data-value="mins"]');
+    this.hours = document.querySelector('.value[data-value="hours"]');
+    this.days = document.querySelector('.value[data-value="days"]');
+
+    this.action = this.action.bind(this);
+    this.start = this.start.bind(this);
+    this.stop = this.stop.bind(this);
+  }
+
+  action() {
+    const currentTime = Date.now();
+    const time = this.date - currentTime;
+    const sec = Math.floor((time % (1000 * 60)) / 1000);
+    const minutes = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+    const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const days = Math.floor(time / (1000 * 60 * 60 * 24));
+
+    this.sec.textContent = sec < 10 ? `0${sec}` : sec;
+    this.min.textContent = minutes < 10 ? `0${minutes}` : minutes;
+    this.hours.textContent = hours < 10 ? `0${hours}` : hours;
+    this.days.textContent = days < 10 ? `0${days}` : days;
+  }
+
+  start() {
+    this.timerId = setInterval(this.action, 1000);
+  }
+
+  stop() {
+    clearInterval(this.timerId);
+  }
+
+  init() {
+    this.startBtn.addEventListener("click", this.start);
+    this.stopBtn.addEventListener("click", this.stop);
   }
 }
-const time = new CountdownTimer("Jul 17, 2020");
 
-let watchId = null;
-let startDate = null;
-let result = null;
-let seconds = null;
-let minutes = null;
-
-function timer() {
-  result = Math.floor((new Date() - startDate) / 1000);
-  seconds = result % 60;
-  minutes = Math.floor(result / 60);
-  refs.secs.textContent = seconds < 10 ? `0${seconds}` : seconds;
-  refs.mins.textContent = minutes < 10 ? `0${minutes}` : minutes;
-}
-
-function startWhatch() {
-  startDate = new Date();
-  watchId = setInterval(timer, 1000);
-}
-
-function stopWhatch() {
-  clearInterval(watchId);
-}
-refs.startBtn.addEventListener("click", startWhatch);
-refs.stopBtn.addEventListener("click", stopWhatch);
+const timer = new CountdownTimer({
+  date: new Date("Aug 29, 2021 07:00:40"),
+});
+timer.init();
 
 // ================ Простой метод
-
+// const refs = {
+//   startBtn: document.querySelector("#start"),
+//   stopBtn: document.querySelector("#stop"),
+//   days: document.querySelector('.value[data-value="days"]'),
+//   hours: document.querySelector('.value[data-value="hours"]'),
+//   mins: document.querySelector('.value[data-value="mins"]'),
+//   secs: document.querySelector('.value[data-value="secs"]'),
+//   timers: document.getElementById("timer-1"),
+// };
 // const targetDate = new Date("Aug 29, 2021 07:00:40").getTime();
 // const CountdownTimer = setInterval(function () {
 //   let date = new Date().getTime();
